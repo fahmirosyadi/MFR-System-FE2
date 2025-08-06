fd744123ee30908c13509e657536d53cb421ba69
Muhammad Fahmi Rosyadi <21020268@ad.sakura-system.co.id>
2025-08-06 19:41:15 +0700

    Add menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import { AbstractComponent } from '../abstract/abstract.component';
 import { SharedModule } from '../shared.module';
+import { CommonService } from '../services/common.service';
+import { DialogService } from '../services/dialog.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MenuDetailComponent } from '../menu-detail/menu-detail.component';
 
 interface FoodNode {
   name: string;
@@ -13,32 +17,55 @@ interface FoodNode {
   templateUrl: './menu.component.html'
 })
 
-export class MenuComponent extends AbstractComponent{
+export class MenuComponent extends AbstractComponent implements OnInit{
 
-  dataSource = EXAMPLE_DATA;
+  public dataSource: any;
+
+  public url = "menu";
+
+  constructor(public cs: CommonService, public dialog: MatDialog, private dialogService: DialogService){
+    super();
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.dataSource = await this.cs.get(this.url);
+  }
 
   childrenAccessor = (node: FoodNode) => node.children ?? [];
 
   hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 
-}
+  async refresh() {
+    // this.dataSource.data = await this.cs.get(this.url);
+  }
 
-const EXAMPLE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
-  },
-  {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-      },
-      {
-        name: 'Orange',
-        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-      },
-    ],
-  },
-];
+  async openDialog(data: any){
+    const dialogRef = this.dialog.open(MenuDetailComponent, {
+      width: '400px',
+      data: { 
+        url: this.url,
+        data: data,
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.refresh(); // Call refresh after dialog is closed
+    });
+  };
+
+  add(data: any){
+    
+  }
+
+  delete = (data: any) => {
+    // let self = this;
+    // this.dialogService.confirm('Are you sure you want to delete?').subscribe((result: any) => {
+    //   if (result) {
+    //     this.cs.delete(this.url + "/" + data.id, async () => {
+    //       this.refresh();
+    //     })
+    //   }
+    // });    
+  }
+
+}
